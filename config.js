1c2997b678943e9f76538f7c90137bec9e16f36e
puffino26 <maxjr0030@gmail.com>
2025-03-31 01:48:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-    ['393762030481', 'Proprietario', true],
+    ['6281216791516', 'Proprietario', true],
 ] //Numero degli owner
 
 //────────────────────────────
